1e3268f4406314a33bf545a1cdb0415272c2f9db
Aleks-E <aleks99979@bk.ru>
2020-11-06 03:07:14 +0300

    added task_2

---
+++ hw1/test_task_1.py
@@ -0,0 +1,24 @@
+import pytest
+
+# from calculator.calc import check_power_of_2
+from hw1.task_1 import check_power_of_2
+
+
+@pytest.mark.parametrize(
+    ["value", "expected_result"],
+    [
+        (65536, True),
+        (65535, False),
+        (12, False),
+        (1, True),
+        (0, True),
+    ],
+)
+def test_power_of_2(value: int, expected_result: bool):
+    actual_result = check_power_of_2(value)
+
+    assert actual_result == expected_result
+
+
+
+
