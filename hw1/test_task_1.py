f6559ed7dddb4b4c4ab4c3333680977f96e6d601
Aleks-E <aleks99979@bk.ru>
2020-11-06 03:13:43 +0300

    replaced test_task_1.py

--- hw1/test_task_1.py
+++ hw1/test_task_1.py
@@ -1,6 +1,5 @@
 import pytest
 
-# from calculator.calc import check_power_of_2
 from hw1.task_1 import check_power_of_2
 
 
@@ -18,7 +17,3 @@ def test_power_of_2(value: int, expected_result: bool):
     actual_result = check_power_of_2(value)
 
     assert actual_result == expected_result
-
-
-
-
