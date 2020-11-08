ecb32ce199720a2dc495bd39b897c0260d1feb8f
Aleks-E <aleks99979@bk.ru>
2020-11-08 13:28:16 +0300

    fix

--- hw1/task_4.py
+++ hw1/task_4.py
@@ -11,10 +11,10 @@ from typing import List
 
 def check_sum_of_four(a: List[int], b: List[int], c: List[int], d: List[int]) -> int:
     zero_numbers = 0
-    for i in a:
-        for j in b:
-            for k in c:
-                for n in d:
+    for i in a[0]:
+        for j in b[1]:
+            for k in c[2]:
+                for n in d[3]:
                     if i + j + k + n == 0:
                         zero_numbers += 1
     return zero_numbers
