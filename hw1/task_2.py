e1fac6e70558393c61006b4a2cea3c5aa71f4abe
Aleks-E <aleks99979@bk.ru>
2020-11-08 16:45:13 +0300

    fixed files

--- hw1/task_2.py
+++ hw1/task_2.py
@@ -9,13 +9,19 @@ We guarantee, that the given sequence contain >= 0 integers inside.
 from typing import List
 
 
+def fib_gen():
+    number_prev_1, number_prev = 0, 1
+    yield number_prev_1
+    yield number_prev
+    while True:
+        number = number_prev + number_prev_1
+        number_prev_1, number_prev = number_prev, number
+        yield number
+
+
 def check_fibonacci(data: List[int]) -> bool:
-    if data == [0]:
-        return True
-    if data == [0, 1]:
-        return True
-    while data[2:]:
-        if data[2] != data[0] + data[1]:
+    gen = fib_gen()
+    for i in data:
+        if i != next(gen):
             return False
-        data = data[1:]
     return True
