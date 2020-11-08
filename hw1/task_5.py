b297262043ea5b3f80af504819fcdd9cb4031d20
Aleks-E <aleks99979@bk.ru>
2020-11-08 17:26:47 +0300

    fixed files

--- hw1/task_5.py
+++ hw1/task_5.py
@@ -13,16 +13,15 @@ from typing import List
 
 
 def find_maximal_subarray_sum(nums: List[int], k: int) -> int:
-    """ Function calculates sum of sub-array elements """
-    if k > len(nums):
-        return -1
-    if k <= 0:
-        return -1
-    subarray_sum_maximum = subarray_sum_current = sum(nums[0:k])
-    while len(nums) > k:
-        subarray_sum_current -= nums[0]
-        nums = nums[1:]
-        subarray_sum_current += nums[k - 1]
-        if subarray_sum_current > subarray_sum_maximum:
-            subarray_sum_maximum = subarray_sum_current
-    return subarray_sum_maximum
+    if len(nums) == 1 and k == 1:
+        return len[0]
+    if len(nums) >= k and k > 0:
+        subarray_sum_maximum = subarray_sum_current = sum(nums[0:k])
+        while len(nums) > k:
+            subarray_sum_current -= nums[0]
+            nums = nums[1:]
+            subarray_sum_current += nums[k - 1]
+            if subarray_sum_current > subarray_sum_maximum:
+                subarray_sum_maximum = subarray_sum_current
+        return subarray_sum_maximum
+    return -1
