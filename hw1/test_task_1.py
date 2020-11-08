632f48b94ee11c09895e0364c1d19ac455d5134d
Aleks-E <aleks99979@bk.ru>
2020-11-08 14:10:18 +0300

    fixed files

--- hw1/test_task_1.py
+++ hw1/test_task_1.py
@@ -5,13 +5,13 @@ from hw1.task_1 import check_power_of_2
 
 @pytest.mark.parametrize(
     ("value", "expected_result"),
-    (
+    [
         (65536, True),
         (65535, False),
         (12, False),
         (1, True),
         (0, True),
-    ),
+    ],
 )
 def test_power_of_2(value: int, expected_result: bool):
     actual_result = check_power_of_2(value)
