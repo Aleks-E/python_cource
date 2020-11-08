e0c974757ab4a5cdac9391ce94a2f3219194c43f
Aleks-E <aleks99979@bk.ru>
2020-11-08 14:48:41 +0300

    fixed files

--- hw1/task_2.py
+++ hw1/task_2.py
@@ -6,9 +6,10 @@ Given a cell with "it's a fib sequence" from slideshow,
 We guarantee, that the given sequence contain >= 0 integers inside.
 
 """
+from typing import List
 
 
-def check_fibonacci(data: list[int]) -> bool:
+def check_fibonacci(data: List[int]) -> bool:
     if data == [0]:
         return True
     if data == [0, 1]:
