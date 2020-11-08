d4683da23da91994c10b00cf6af6be53290eaf63
Aleks-E <aleks99979@bk.ru>
2020-11-08 22:11:07 +0300

    added dir

--- hw1/test_task_1.py
+++ hw1/test_task_1.py
@@ -7,10 +7,13 @@ from hw1.task_1 import check_power_of_2
     ["value", "expected_result"],
     [
         (65536, True),
-        (65535, False),
-        (12, False),
+        (16384, True),
+        (16, True),
         (1, True),
         (0, True),
+        (50000, False),
+        (10000, False),
+        (1000, False)
     ],
 )
 def test_power_of_2(value: int, expected_result: bool):
