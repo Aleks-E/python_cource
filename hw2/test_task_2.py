3f6ee7776ff9b92c4f663f394ba0f8c969000674
Aleks-E <aleks99979@bk.ru>
2020-11-10 00:53:13 +0300

    added files

--- hw2/test_task_2.py
+++ hw2/test_task_2.py
@@ -1,15 +1,15 @@
+from typing import List, Tuple
+
 import pytest
 
 from hw2.task_2 import major_and_minor_elem
-
-from typing import List, Tuple
 
 data = [2, 1, 3, 2, 3, 2]
 result = (2, 1)
 
 
 @pytest.mark.parametrize(
-    ["inp", "expected_result"],
+    ("inp", "expected_result"),
     [
         (data, result),
     ],
