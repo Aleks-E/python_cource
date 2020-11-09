565367c5cab19fce776bd39bcdcb8bb7a851eabb
Aleks-E <aleks99979@bk.ru>
2020-11-09 23:20:20 +0300

    fixed files

--- hw2/task_1.py
+++ hw2/task_1.py
@@ -16,13 +16,13 @@ def get_longest_diverse_words(file_path: str) -> List[str]:
     with open("file_path", "r") as data:
         for line in data:
             line = line.rstrip()
-            line = line.split(' ')
+            line = line.split(" ")
             for word in line:
-                if word != '':
-                    word = word.encode().decode('unicode-escape')
+                if word != "":
+                    word = word.encode().decode("unicode-escape")
                     word = word.translate(word.maketrans("", "", string.punctuation))
-                    word = word.replace('»', '')
-                    word = word.replace('«', '')
+                    word = word.replace("»", "")
+                    word = word.replace("«", "")
                     if word not in words_unique:
                         words_unique[word] = len(set(word))
 
@@ -35,19 +35,11 @@ def get_longest_diverse_words(file_path: str) -> List[str]:
         return longest_unique_words
 
 
-
-
-
 # def get_rarest_char(file_path: str) -> str:
 #     chars = {}
 #     with open('data.txt', 'r') as data:
 #         for line in data:
 #             line = line.rstrip()
-
-
-
-
-
 
 
 def count_punctuation_chars(file_path: str) -> int:
@@ -62,7 +54,4 @@ def get_most_common_non_ascii_char(file_path: str) -> str:
     ...
 
 
-
-
 # print(get_longest_diverse_words('data.txt'))
-
--- hw2/test_task_1.py
+++ hw2/test_task_1.py
@@ -3,12 +3,11 @@ import pytest
 from hw2.task_1 import get_longest_diverse_words
 
 with open("hw2/test_task_1_data.txt", "w") as test_data:
-    test_data.write("aaaaa\n"
-                    "tabc\n"
-                    "abd fgthy abf ab.g wey fvs qhy zxc. hjy")
+    test_data.write("aaaaa\n" "tabc\n" "abd fgthy abf ab.g wey fvs qhy zxc. hjy")
 
 
-result = ['fgthy', 'tabc', 'abd', 'abf', 'abg', 'wey', 'fvs', 'qhy', 'zxc', 'hjy']
+result = ["fgthy", "tabc", "abd", "abf", "abg", "wey", "fvs", "qhy", "zxc", "hjy"]
+
 
 @pytest.mark.parametrize(
     ["file_name", "expected_result"],
@@ -16,9 +15,7 @@ result = ['fgthy', 'tabc', 'abd', 'abf', 'abg', 'wey', 'fvs', 'qhy', 'zxc', 'hjy
         ("hw2/test_task_1_data.txt", result),
     ],
 )
-
 def test_get_longest_diverse_words(file_name: str, expected_result: list):
     actual_result = get_longest_diverse_words(file_name)
 
     assert actual_result == expected_result
-
