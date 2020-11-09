edf1f656d0fa847f8cf11f1cbeb6681d5cc0ef65
Aleks-E <aleks99979@bk.ru>
2020-11-09 23:15:01 +0300

    run test

--- hw2/test_task_1.py
+++ hw2/test_task_1.py
@@ -2,10 +2,10 @@ import pytest
 
 from hw2.task_1 import get_longest_diverse_words
 
-# with open("hw2/test_task_1_data.txt", "w") as test_data:
-#     test_data.write("aaaaa\n"
-#                     "tabc\n"
-#                     "abd fgthy abf ab.g wey fvs qhy zxc. hjy")
+with open("hw2/test_task_1_data.txt", "w") as test_data:
+    test_data.write("aaaaa\n"
+                    "tabc\n"
+                    "abd fgthy abf ab.g wey fvs qhy zxc. hjy")
 
 
 result = ['fgthy', 'tabc', 'abd', 'abf', 'abg', 'wey', 'fvs', 'qhy', 'zxc', 'hjy']
