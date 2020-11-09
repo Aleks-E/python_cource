a6d667e437b6a505f44ecdb8004317a3b0a2e430
Aleks-E <aleks99979@bk.ru>
2020-11-10 00:06:32 +0300

    fixed files

--- hw2/task_1.py
+++ hw2/task_1.py
@@ -32,7 +32,7 @@ def get_longest_diverse_words(file_path: str) -> List[str]:
             longest_unique_words.append(words_list[i][0])
 
         return longest_unique_words
-print(11)
+
 
 def get_rarest_char(file_path: str) -> str:
     ...
