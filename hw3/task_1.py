c413952ab3d1345ef904f69758982b99f05dd19d
Aleks-E <aleks99979@bk.ru>
2020-11-15 18:30:03 +0300

    fixed files

--- hw3/task_1.py
+++ hw3/task_1.py
@@ -1,48 +1,60 @@
-# def decorator(argument):
+# def dec_2(n):
 #     def dec_1(func):
+#         count = n
 #         def inner():
-#             print(argument)
-#             print('12')
-#             func()
+#             nonlocal count
+#             a = 0
+#             while True:
+#                 while count:
+#                     if count == n:
+#                         print('ok_1')
+#                         func()
+#                         count -= 1
+#                         return n
+#                     else:
+#                         func()
+#                         count -= 1
+#                         return n
+#                 count = n
 #
 #         return inner
 #     return dec_1
 #
 #
-# @decorator(34)
+# @dec_2(3)
 # def func():
-#     print('ok')
+#     return int(input('? '))
 #
-# func()      # 34
-#             # 12
-#             # ok
+#
+# func()      # ok_1
+#             # ?
+# func()      # ?
+# func()      # ?
 
-# ---------------------------
-
+# ---------------------------------
 
 def dec_2(n):
     def dec_1(func):
         count = n
         def inner():
             nonlocal count
-            a = 0
+            # a = 0
             while True:
+                print('in while_1')
+                a = func()
+                # print(a)
                 while count:
+                    print('in while_2')
                     if count == n:
                         # print('ok_1')
                         # func()
-                        a = func()
-                        print(a)
-
                         count -= 1
-                        return
-                        # return n
+                        print(a)
+                        # return a
                     else:
                         # func()
-                        print(a)
                         count -= 1
-                        return
-                        # return n
+                        return a
                 count = n
 
         return inner
@@ -54,17 +66,41 @@ def func():
     return int(input('? '))
 
 
-func()      # ok_1
-            # ok
+print(func())
+# print(func())
+# print(func())
+# print(func())
+# print(func())
+# print(func())
+# print(func())
+# print(func())
+# print(func())
+# print(func())
 
-func()      # ok
-func()      # ok
 
-func()      # ok_1
-            # ok
+# func()      # ok_1
+#             # ?
+# func()      # ?
+# func()      # ?
+#
+# func()      # ok_1
+#             # ?
+# func()      # ?
+# func()      # ?
 
-func()      # ok
-func()      # ok
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
