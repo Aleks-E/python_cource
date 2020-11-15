b32cbb01d4bf612d09b120941d2b623c69ca69b3
Aleks-E <aleks99979@bk.ru>
2020-11-15 14:54:10 +0300

    fixed file

--- hw3/task_4.py
+++ hw3/task_4.py
@@ -1,7 +1,7 @@
 def is_armstrong(number: int) -> bool:
     number_length = len(str(number))
     power_sum = 0
-    for i in str(number):
-        power_sum += int(i) ** number_length
+    for digit in str(number):
+        power_sum += int(digit) ** number_length
 
     return power_sum == number
