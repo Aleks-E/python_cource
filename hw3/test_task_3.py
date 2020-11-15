39021be420526e8343cb8092659efc4f12cf0227
Aleks-E <aleks99979@bk.ru>
2020-11-15 19:01:35 +0300

    changed tests

--- hw3/test_task_3.py
+++ hw3/test_task_3.py
@@ -1,46 +1,40 @@
 from hw3.task_3 import make_filter
 
-import pytest
 
-
-right_input_and_filter_data = [
-    {"name": "Bill", "last_name": "Gilbert"},
-    {"name": "polly", "type": "bird"},
-]
-result_right_input_and_filter_data = [{"name": "polly", "type": "bird"}]
-
-incorrect_input_initial_key = [
-    {"name": "Bill", "last_name": "Gilbert"},
-    {"name_1": "polly", "type": "bird"},
-]
-result_incorrect_input_initial_key = []
-
-incorrect_input_initial_value = [
-    {"name": "Bill", "last_name": "Gilbert"},
-    {"name": "polly_1", "type": "bird"},
-]
-result_incorrect_input_initial_value = []
-
-missing_input_value = [{"name": "Bill", "last_name": "Gilbert"}, {"name": "polly"}]
-result_missing_input_value = []
-
-missing_input_item = [{"name": "Bill", "last_name": "Gilbert"}]
-result_missing_input_item = []
-
-
-@pytest.mark.parametrize(
-    ("data", "expected_result"),
-    [
-        (right_input_and_filter_data, result_right_input_and_filter_data),
-        (incorrect_input_initial_key, result_incorrect_input_initial_key),
-        (incorrect_input_initial_value, result_incorrect_input_initial_value),
-        (missing_input_value, result_missing_input_value),
-        (missing_input_item, result_missing_input_item),
-    ],
-)
-def test_make_filter(data, expected_result):
+def test_matching_input_data_and_filter_data():
+    data = [
+        {"name": "Bill", "last_name": "Gilbert"},
+        {"name": "polly", "type": "bird"},
+    ]
+    expected_result = [{"name": "polly", "type": "bird"}]
     actual_result = make_filter(name="polly", type="bird").apply(data)
     assert actual_result == expected_result
+
+
+def test_mismatching_input_data_key_and_filter_data_key():
+    data = [
+        {"name": "Bill", "last_name": "Gilbert"},
+        {"name_1": "polly", "type": "bird"},
+    ]
+    assert [] == make_filter(name="polly", type="bird").apply(data)
+
+
+def test_mismatching_input_data_value_and_filter_data_value():
+    data = [
+        {"name": "Bill", "last_name": "Gilbert"},
+        {"name": "polly_1", "type": "bird"},
+    ]
+    assert [] == make_filter(name="polly", type="bird").apply(data)
+
+
+def test_missing_key_filters_item():
+    data = [{"name": "Bill", "last_name": "Gilbert"}, {"name": "polly"}]
+    assert [] == make_filter(name="polly", type="bird").apply(data)
+
+
+def test_missing_input_data_item():
+    data = [{"name": "Bill", "last_name": "Gilbert"}]
+    assert [] == make_filter(name="polly", type="bird").apply(data)
 
 
 def test_wrong_input_filter_key():
