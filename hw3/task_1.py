7dc3cb459cc31646a2ebd75775370cd6ee77f696
Aleks-E <aleks99979@bk.ru>
2020-11-19 02:31:58 +0300

    added hw 3 task 1

--- hw3/task_1.py
+++ hw3/task_1.py
@@ -1,27 +1,26 @@
-def number_of_iter_set(number_of_iterations: int) -> object:
-    def cache(func: object) -> object:
+from collections.abc import Callable
+
+
+def cache(number_of_iterations: int) -> Callable:
+    def iter_set(func: Callable) -> Callable:
+        cached = []
         count_iteration = number_of_iterations
-        cached_number = 0
 
-        def iter_check() -> int:
+        def cached_func(*args) -> object:
             nonlocal count_iteration
-            nonlocal cached_number
-            while True:
-                while count_iteration:
-                    if count_iteration == number_of_iterations:
-                        cached_number = func()
+
+            while count_iteration:
+                for stored_args, result in cached:
+                    if stored_args == args:
                         count_iteration -= 1
-                        return cached_number
-                    else:
-                        count_iteration -= 1
-                        return cached_number
+                        return result
+
+                result = func(*args)
+                cached.append((args, result))
                 count_iteration = number_of_iterations
+                count_iteration -= 1
+                return result
 
-        return iter_check
+        return cached_func
 
-    return cache
-
-
-@number_of_iter_set(number_of_iterations=4)
-def input_number() -> int:
-    return int(input("? "))
+    return iter_set
---
+++ hw3/test_task_1.py
@@ -0,0 +1,29 @@
+from hw3.task_1 import cache
+
+
+@cache(number_of_iterations=2)
+def funk(number):
+    return number + 1
+
+
+def test_cache_number_of_iterations():
+    arg_of_func = 2
+    results = []
+    result_call_of_func_1 = funk(arg_of_func)
+    results.append(result_call_of_func_1)
+
+    result_call_of_func_2 = funk(arg_of_func)
+    results.append(result_call_of_func_2)
+
+    result_call_of_func_3 = funk(arg_of_func)
+    results.append(result_call_of_func_3)
+
+    assert results == [3, 3, None]
+
+
+def test_cache_id_of_numbers():
+    arg_of_func = 2
+    result_call_of_func_1 = funk(arg_of_func)
+    result_call_of_func_2 = funk(arg_of_func)
+
+    assert result_call_of_func_1 is result_call_of_func_2
