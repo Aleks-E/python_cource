48fcb4fb86f4eb2b3ae76df78c0fee78f744da95
Aleks-E <aleks99979@bk.ru>
2020-11-28 23:30:19 +0300

    homework 6 task 1

--- hw6/task_1.py
+++ hw6/task_1.py
@@ -50,71 +50,50 @@ PEP8 соблюдать строго.
 давать логичные подходящие имена.
 """
 import datetime
-from typing import Callable
 from collections import defaultdict
+from typing import TypeVar
+
+
+class ObjectError(Exception):
+    ...
+
 
 class DeadlineError(Exception):
     ...
 
-class Personal_Info:
-    def __init__(self, first_name: str, last_name: str):        # ok
+
+homework_result = TypeVar("homework_result", bound="HomeworkResult")
+teacher = TypeVar("teacher", bound="Teacher")
+
+
+class PersonalInfo:
+    def __init__(self, first_name: str, last_name: str):
         self.first_name = first_name
         self.last_name = last_name
 
 
-class Teacher(Personal_Info):
-    homework_done = defaultdict(list)
-
-    def create_homework(self, text: str, days: int) -> Callable:            # ok
-        return Homework(text, days)
-
-    @classmethod
-    def check_homework(cls, result):
-        if len(result.solution) > 5:
-            if result.homework not in cls.homework_done:
-                cls.homework_done[result.homework].append(result)
-            elif result not in cls.homework_done[result.homework]:
-                cls.homework_done[result.homework].append(result)
-
-            # cls.homework_done[result.homework].append(result)
-            return True
-        else:
-            return False
-
-    @classmethod
-    def reset_results(cls, homework=None):
-        if homework is None:
-            cls.homework_done.clear()
-        else:
-            del cls.homework_done[homework]
-
-
 class Homework:
-    def __init__(self, text: str, deadline: int):       # ok
+    def __init__(self, text: str, deadline: int):
         self.text = text
         self.deadline = datetime.timedelta(deadline)
         self.created = datetime.datetime.now()
 
-    def is_active(self) -> bool:                        # ok
+    def is_active(self) -> bool:
         return self.created + self.deadline > datetime.datetime.now()
 
 
-class Student(Personal_Info):
-    def do_homework(self, homework: Homework, solution: str) -> Callable:       # ok
-        # if not homework.is_active():
-        #     raise DeadlineError('You are late')
-
-        # return HomeworkResult(homework, self, solution)
-        result = HomeworkResult(homework, self, solution)           # Review
+class Student(PersonalInfo):
+    def do_homework(self, homework: Homework, solution: str) -> homework_result:
+        result = HomeworkResult(homework, self, solution)
         if not homework.is_active():
-            raise DeadlineError('You are late')
+            raise DeadlineError("You are late")
         return result
 
 
 class HomeworkResult:
-    def __init__(self, homework: Homework, author: Student, solution: str):     # ok
+    def __init__(self, homework: Homework, author: Student, solution: str):
         if not isinstance(homework, Homework):
-            raise Exception('You gave a not Homework object')
+            raise ObjectError("You gave a not Homework object")
         else:
             self.homework = homework
             self.author = author
@@ -122,250 +101,22 @@ class HomeworkResult:
             self.created = datetime.datetime.now()
 
 
-
-"""
-1 Домашнее задание, переданное студенту, неправильного типа
-
-
-
-
-1 Просрочено домашнее задание
-
-
-1 Результат записался в homework_done
-2 Результат содержит правильный ответ
-3 Результат принадлежит конкретному студенту
-4 Результат относится к конкретной работе
-
-
-"""
-
-
-
-
-
-
-
-
-
-
-
-# opp_teacher = Teacher('Daniil', 'Shadrin')
-# print('Teacher_personal_info\t', opp_teacher.first_name, opp_teacher.last_name)
-#
-# lazy_student = Student('Roman', 'Petrov')
-# print('Student_personal_info\t', lazy_student.first_name, lazy_student.last_name)
-#
-# oop_hw = opp_teacher.create_homework('Learn OOP', 0)
-# # result = lazy_student.do_homework(lazy_student, 'I have done this hw')       # Exception: You gave a not Homework object
-#
-# # result = lazy_student.do_homework(oop_hw, 'I have done this hw')            #     raise DeadlineError('You are late')
-#                                                                             # Exception: You are late
-#
-# oop_hw = opp_teacher.create_homework('Learn OOP', 1)
-# print('oop_hw\t', oop_hw)
-# print('oop_hw_attr\t', oop_hw.text, oop_hw.deadline, oop_hw.created)
-#
-#
-# result = lazy_student.do_homework(oop_hw, '12345')
-# print('result_1\t', result)
-# print('result_1_attr\t', result.homework, result.author, result.solution, result.created)
-#
-# print(opp_teacher.check_homework(result))     # False
-# print(opp_teacher.homework_done)                # defaultdict(None, {})
-#
-#
-#
-# result = lazy_student.do_homework(oop_hw, 'I have done this hw')
-# print(opp_teacher.check_homework(result))     # True
-# print(opp_teacher.homework_done)                # defaultdict(None, {<__main__.HomeworkResult object at 0x0000000002148208>: True})
-#
-#
-# advanced_python_teacher = Teacher('Aleksandr', 'Smetanin')
-# good_student = Student('Lev', 'Sokolov')
-# docs_hw = opp_teacher.create_homework('Read docs', 5)
-#
-# result_2 = good_student.do_homework(docs_hw, 'I have done this hw too')
-# result_3 = lazy_student.do_homework(docs_hw, 'done   ')
-#
-# opp_teacher.check_homework(result_2)
-# opp_teacher.check_homework(result_2)
-# opp_teacher.check_homework(result_3)
-#
-#
-# print(advanced_python_teacher.homework_done)
-#
-#
-# for i in advanced_python_teacher.homework_done:
-#     print('i\t', i, advanced_python_teacher.homework_done[i])
-#
-#
-# Teacher.reset_results()
-# print(advanced_python_teacher.homework_done)
-#
-# opp_teacher.check_homework(result_2)
-# print(advanced_python_teacher.homework_done)
-#
-# opp_teacher.check_homework(result_3)
-# opp_teacher.check_homework(result)
-# print(advanced_python_teacher.homework_done)
-#
-# Teacher.reset_results(oop_hw)
-# print(advanced_python_teacher.homework_done)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# ---------------------------------
-# def func(arg):
-#     if arg < 0:
-#         raise Exception('www')
-#
-# # func(-4)
-# ---------------------------------
-
-
-
-
-
-# ---------------------------------
-# class MyClass:
-#     @staticmethod
-#     def ex_static_method():
-#         print("static method")
-#
-#     @classmethod
-#     def ex_class_method(cls):
-#         print("class method")
-# ---------------------------------
-
-
-
-# defdict = defaultdict(list)
-#
-# class A:
-#     my_list = []
-#
-#     # @classmethod
-#     def adder(cls, arg):
-#         cls.my_list.append(arg)
-
-
-# a_1 = A()
-# a_2 = A()
-#
-# print(A.my_list)
-# print(a_1.my_list)
-# print(a_2.my_list)
-#
-# a_1.adder(1)
-# print(A.my_list)
-# print(a_1.my_list)
-# print(a_2.my_list)
-#
-# a_2.adder(2)
-# print(A.my_list)
-# print(a_1.my_list)
-# print(a_2.my_list)
-#
-# print(a_1.my_list is a_2.my_list)
-# ------------------------------
-
-
-
-# ====================================================================
-# ------------------------------
-
-# defdict = defaultdict(list)
-#
-# for i in range(5):
-#     defdict[i].append(i)
-#
-# print(defdict)
-
-# ------------------------------
-
-# my_str = 'qwertyqw'
-# d = defaultdict(int)
-# for key in my_str:
-#     d[key] += 1
-#
-# print(d)
-
-# ------------------------------
-
-# d = defaultdict(int)
-# d['a'] += 3
-# print(d)
-
-# ---------------------------
-
-# d = defaultdict(list)
-# d['a'].append(3)
-# d['a'] += [5, 7]
-# print(d)
-
-# ---------------------------
-
-# d = defaultdict(int)
-# print(d['r'])               # 0
-
-# ---------------------------
-
-# def a():
-#     return 22
-#
-# defdict = defaultdict(a)
-#
-# print(defdict['q'])     # 22
-# ---------------------------
-
-# class A:
-#     def a(self):
-#         return 33
-#
-#
-# defdict = defaultdict(A().a)
-#
-# print(defdict['q'])     # 33
-# print(defdict)
-
-# ---------------------------
-
-# defdict = defaultdict(bool)
-#
-# # defdict('1')
-#
-# print(defdict['1'])
-# print(defdict)
-
-# ---------------------------
-
-
-
-
-# defdict = defaultdict(list)
-#
-#
-# defdict['1']
-#
-#
-# print(defdict)
-
-
-
-
-
-
-
-
+class Teacher(PersonalInfo):
+    homework_done = defaultdict(list)
+
+    def create_homework(self, text: str, days: int) -> Homework:
+        return Homework(text, days)
+
+    @classmethod
+    def check_homework(cls: teacher, result: HomeworkResult) -> bool:
+        if len(result.solution) > 5:
+            if not (result in cls.homework_done[result.homework]):
+                cls.homework_done[result.homework].append(result)
+            return True
+        return False
+
+    @classmethod
+    def reset_results(cls: teacher, homework: Homework = None) -> None:
+        cls.homework_done.clear() if homework is None else cls.homework_done.pop(
+            homework
+        )
--- hw6/test_task_1.py
+++ hw6/test_task_1.py
@@ -1,147 +1,188 @@
 import datetime
-from hw6.task_1 import Teacher, Student, DeadlineError, HomeworkResult
+
+from hw6.task_1 import DeadlineError, HomeworkResult, ObjectError, Student, Teacher
+
 import pytest
 
 
+def test_teacher_attribute():
+    first_name, last_name = "Daniil", "Shadrin"
+    teacher = Teacher(first_name, last_name)
+    assert teacher.first_name == first_name
+    assert teacher.last_name == last_name
+
+
+def test_student_attribute():
+    first_name, last_name = "Roman", "Petrov"
+    student = Student(first_name, last_name)
+    assert student.first_name == first_name
+    assert student.last_name == last_name
+
+
 def test_wrong_object_of_homework():
-    lazy_student = Student('Roman', 'Petrov')
-    with pytest.raises(Exception, match="You gave a not Homework object"):
-        lazy_student.do_homework(1, 'solution')
+    lazy_student = Student("Roman", "Petrov")
+    with pytest.raises(ObjectError, match="You gave a not Homework object"):
+        lazy_student.do_homework(1, "solution")
 
 
 def test_expired_deadline_of_homework():
-    opp_teacher = Teacher('Daniil', 'Shadrin')
-    lazy_student = Student('Roman', 'Petrov')
+    opp_teacher = Teacher("Daniil", "Shadrin")
+    lazy_student = Student("Roman", "Petrov")
     deadline = 0
-    oop_hw = opp_teacher.create_homework('Learn OOP', deadline)
+    oop_hw = opp_teacher.create_homework("Learn OOP", deadline)
     with pytest.raises(DeadlineError, match="You are late"):
-        lazy_student.do_homework(oop_hw, 'I have done this hw')
+        lazy_student.do_homework(oop_hw, "I have done this hw")
 
 
 def test_type_of_object_returned_by_the_student():
-    opp_teacher = Teacher('Daniil', 'Shadrin')
-    lazy_student = Student('Roman', 'Petrov')
+    opp_teacher = Teacher("Daniil", "Shadrin")
+    lazy_student = Student("Roman", "Petrov")
     deadline = 1
-    oop_hw = opp_teacher.create_homework('Learn OOP', deadline)
-    result = lazy_student.do_homework(oop_hw, 'I have done this hw')
-    # assert result.__eq__(HomeworkResult)
-    assert isinstance(result, HomeworkResult)       # ?
+    oop_hw = opp_teacher.create_homework("Learn OOP", deadline)
+    result = lazy_student.do_homework(oop_hw, "I have done this hw")
+    assert isinstance(result, HomeworkResult)
 
 
 def test_wrong_result_of_the_homework():
-    opp_teacher = Teacher('Daniil', 'Shadrin')
-    lazy_student = Student('Roman', 'Petrov')
+    opp_teacher = Teacher("Daniil", "Shadrin")
+    lazy_student = Student("Roman", "Petrov")
     deadline = 1
-    oop_hw = opp_teacher.create_homework('Learn OOP', deadline)
-    result = lazy_student.do_homework(oop_hw, '12345')
+    oop_hw = opp_teacher.create_homework("Learn OOP", deadline)
+    solution = "12345"
+    result = lazy_student.do_homework(oop_hw, solution)
     assert not opp_teacher.check_homework(result)
     assert not opp_teacher.homework_done
 
 
 def test_right_result_of_the_homework():
-    opp_teacher = Teacher('Daniil', 'Shadrin')
-    lazy_student = Student('Roman', 'Petrov')
+    opp_teacher = Teacher("Daniil", "Shadrin")
+    lazy_student = Student("Roman", "Petrov")
     deadline = 1
-    oop_hw = opp_teacher.create_homework('Learn OOP', deadline)
-    result = lazy_student.do_homework(oop_hw, '123456')
+    oop_hw = opp_teacher.create_homework("Learn OOP", deadline)
+    solution = "123456"
+    result = lazy_student.do_homework(oop_hw, solution)
     assert opp_teacher.check_homework(result)
     assert opp_teacher.homework_done[oop_hw] == [result]
 
 
 def test_the_same_homework_completed_by_two_students():
-    opp_teacher = Teacher('Daniil', 'Shadrin')
-    lazy_student = Student('Roman', 'Petrov')
-    good_student = Student('Lev', 'Sokolov')
+    opp_teacher = Teacher("Daniil", "Shadrin")
+    lazy_student = Student("Roman", "Petrov")
+    good_student = Student("Lev", "Sokolov")
     deadline = 1
-    oop_hw = opp_teacher.create_homework('Learn OOP', deadline)
-    result_1 = lazy_student.do_homework(oop_hw, '123456')
-    result_2 = good_student.do_homework(oop_hw, '123456')
+    oop_hw = opp_teacher.create_homework("Learn OOP", deadline)
+    solution = "123456"
+    result_1 = lazy_student.do_homework(oop_hw, solution)
+    result_2 = good_student.do_homework(oop_hw, solution)
     opp_teacher.check_homework(result_1)
     opp_teacher.check_homework(result_2)
     assert opp_teacher.homework_done[oop_hw] == [result_1, result_2]
 
 
 def test_different_homework_completed_by_different_students():
-    opp_teacher = Teacher('Daniil', 'Shadrin')
-    lazy_student = Student('Roman', 'Petrov')
-    good_student = Student('Lev', 'Sokolov')
+    opp_teacher = Teacher("Daniil", "Shadrin")
+    lazy_student = Student("Roman", "Petrov")
+    good_student = Student("Lev", "Sokolov")
     deadline = 1
-    oop_hw = opp_teacher.create_homework('Learn OOP', deadline)
-    docs_hw = opp_teacher.create_homework('Read docs', deadline)
-    result_1 = lazy_student.do_homework(oop_hw, '123456')
-    result_2 = good_student.do_homework(docs_hw, '123456')
+    oop_hw = opp_teacher.create_homework("Learn OOP", deadline)
+    docs_hw = opp_teacher.create_homework("Read docs", deadline)
+    solution = "123456"
+    result_1 = lazy_student.do_homework(oop_hw, solution)
+    result_2 = good_student.do_homework(docs_hw, solution)
     opp_teacher.check_homework(result_1)
     opp_teacher.check_homework(result_2)
     assert opp_teacher.homework_done[oop_hw] == [result_1]
     assert opp_teacher.homework_done[docs_hw] == [result_2]
 
 
+def test_different_homework_completed_by_one_student():
+    opp_teacher = Teacher("Daniil", "Shadrin")
+    lazy_student = Student("Roman", "Petrov")
+    deadline = 1
+    oop_hw = opp_teacher.create_homework("Learn OOP", deadline)
+    docs_hw = opp_teacher.create_homework("Read docs", deadline)
+    solution = "123456"
+    result_1 = lazy_student.do_homework(oop_hw, solution)
+    result_2 = lazy_student.do_homework(docs_hw, solution)
+    opp_teacher.check_homework(result_1)
+    opp_teacher.check_homework(result_2)
+    assert opp_teacher.homework_done[oop_hw] == [result_1]
+    assert opp_teacher.homework_done[docs_hw] == [result_2]
+
+
+def test_the_same_homework_result():
+    opp_teacher = Teacher("Daniil", "Shadrin")
+    lazy_student = Student("Roman", "Petrov")
+    deadline = 1
+    oop_hw = opp_teacher.create_homework("Learn OOP", deadline)
+    solution = "123456"
+    result = lazy_student.do_homework(oop_hw, solution)
+    opp_teacher.check_homework(result)
+    opp_teacher.check_homework(result)
+    assert opp_teacher.homework_done[oop_hw] == [result]
+
+
+def test_reset_results_of_definite_homework():
+    opp_teacher = Teacher("Daniil", "Shadrin")
+    lazy_student = Student("Roman", "Petrov")
+    deadline = 1
+    oop_hw = opp_teacher.create_homework("Learn OOP", deadline)
+    docs_hw = opp_teacher.create_homework("Read docs", deadline)
+    solution = "123456"
+    result_1 = lazy_student.do_homework(oop_hw, solution)
+    result_2 = lazy_student.do_homework(docs_hw, solution)
+    opp_teacher.check_homework(result_1)
+    opp_teacher.check_homework(result_2)
+    assert opp_teacher.homework_done[oop_hw] == [result_1]
+    assert opp_teacher.homework_done[docs_hw] == [result_2]
+    opp_teacher.reset_results(oop_hw)
+    assert opp_teacher.homework_done[oop_hw] == []
+    assert opp_teacher.homework_done[docs_hw] == [result_2]
+
+
+def test_reset_results_of_all_homework():
+    opp_teacher = Teacher("Daniil", "Shadrin")
+    lazy_student = Student("Roman", "Petrov")
+    deadline = 1
+    oop_hw = opp_teacher.create_homework("Learn OOP", deadline)
+    docs_hw = opp_teacher.create_homework("Read docs", deadline)
+    solution = "123456"
+    result_1 = lazy_student.do_homework(oop_hw, solution)
+    result_2 = lazy_student.do_homework(docs_hw, solution)
+    opp_teacher.check_homework(result_1)
+    opp_teacher.check_homework(result_2)
+    assert opp_teacher.homework_done[oop_hw] == [result_1]
+    assert opp_teacher.homework_done[docs_hw] == [result_2]
+    opp_teacher.reset_results()
+    assert opp_teacher.homework_done[oop_hw] == []
+    assert opp_teacher.homework_done[docs_hw] == []
+
+
 def test_is_the_list_of_homework_results_common_to_all_teachers():
-    opp_teacher = Teacher('Daniil', 'Shadrin')
-    advanced_python_teacher = Teacher('Aleksandr', 'Smetanin')
+    opp_teacher = Teacher("Daniil", "Shadrin")
+    advanced_python_teacher = Teacher("Aleksandr", "Smetanin")
     assert opp_teacher.homework_done is advanced_python_teacher.homework_done
 
 
+def test_attribute_homework_result():
+    opp_teacher = Teacher("Daniil", "Shadrin")
+    lazy_student = Student("Roman", "Petrov")
+    deadline = 1
+    oop_hw = opp_teacher.create_homework("Learn OOP", deadline)
+    solution = "123456"
+    result = lazy_student.do_homework(oop_hw, solution)
+    assert result.homework == oop_hw
+    assert result.author == lazy_student
+    assert result.solution == solution
 
 
-
-
-
-
-
-
-# def test_teacher_attribute():
-#     first_name, last_name = "Daniil", "Shadrin"
-#     teacher = Teacher(first_name, last_name)
-#     assert teacher.first_name == first_name
-#     assert teacher.last_name == last_name
-#
-#
-# def test_student_attribute():
-#     first_name, last_name = "Roman", "Petrov"
-#     student = Student(first_name, last_name)
-#     assert student.first_name == first_name
-#     assert student.last_name == last_name
-#
-#
-# def test_homework_attribute():
-#     teacher = Teacher("Daniil", "Shadrin")
-#     text_of_homework = "Learn functions"
-#     deadline_days = 1
-#     homework = teacher.create_homework(text_of_homework, deadline_days)
-#     assert homework.text == text_of_homework
-#     assert homework.deadline == datetime.timedelta(days=1)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# ---------------------------
-# from hw6.task_1 import func
-# import pytest
-#
-# def test_func():
-#     with pytest.raises(Exception, match="www"):
-#         func(-4)
-
-
-
-
-
-
-
+def test_attribute_homework():
+    opp_teacher = Teacher("Daniil", "Shadrin")
+    lazy_student = Student("Roman", "Petrov")
+    text_of_homework = "Learn OOP"
+    deadline = 1
+    oop_hw = opp_teacher.create_homework(text_of_homework, deadline)
+    solution = "123456"
+    result = lazy_student.do_homework(oop_hw, solution)
+    assert result.homework.text == text_of_homework
+    assert result.homework.deadline == datetime.timedelta(days=deadline)
