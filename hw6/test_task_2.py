08151f9827190f5770100859f82f1d79be096bc2
Aleks-E <aleks99979@bk.ru>
2020-11-29 21:34:58 +0300

    changed file

--- hw6/test_task_2.py
+++ hw6/test_task_2.py
@@ -4,7 +4,7 @@ from hw6.task_2 import instances_counter
 def test_number_of_created_instanses():
     @instances_counter
     class SomeClass:
-        ...
+        instance_count = 6
 
     assert SomeClass.get_created_instances() == 0
     SomeClass(), SomeClass(), SomeClass()
