09a5a58724b7bd5ee116b989be7e0204badc344c
Aleks-E <aleks99979@bk.ru>
2020-12-01 22:07:53 +0300

    fixed files

--- hw2/test_task_5.py
+++ hw2/test_task_5.py
@@ -4,34 +4,34 @@ import pytest
 
 
 @pytest.mark.parametrize(
-    ("args", "expected_result"),
+    ("iterable", "kwargs", "expected_result"),
     [
-        (("",), ([])),
-        (("0123",), (["0", "1", "2"])),
-        (("012345", "2"), (["2", "3", "4"])),
-        (("012345", "2", "4"), (["2", "3"])),
-        (("012345", "0", "4", 2), (["0", "2"])),
-        (("012345", "4", "1", -2), (["4", "2"])),
+        ("", {}, []),
+        ("0123", {}, ["0", "1", "2"]),
+        ("012345", {"start": "2"}, ["2", "3", "4"]),
+        ("012345", {"start": "2", "stop": "4"}, ["2", "3"]),
+        ("012345", {"start": "0", "stop": "4", "step": 2}, ["0", "2"]),
+        ("012345", {"start": "4", "stop": "1", "step": -2}, ["4", "2"]),
     ],
 )
-def test_of_custom_range_function_work_with_different_parameters(args, expected_result):
-    actual_result = custom_range(*args)
-
+def test_of_custom_range_function_work_with_different_parameters(
+    iterable, kwargs, expected_result
+):
+    actual_result = custom_range(iterable, **kwargs)
     assert actual_result == expected_result
 
 
 @pytest.mark.parametrize(
-    ("args", "expected_result"),
+    ("iterable", "kwargs", "expected_result"),
     [
-        (([],), ([])),
-        (([0, 1, 2, 3, 4, 5], 0, 4, 2), ([0, 2])),
-        (((),), ([])),
-        (((0, 1, 2, 3, 4, 5), 0, 4, 2), ([0, 2])),
+        ([], {}, []),
+        ([0, 1, 2, 3, 4, 5], {"start": 0, "stop": 4, "step": 2}, [0, 2]),
+        ((), {}, []),
+        ((0, 1, 2, 3, 4, 5), {"start": 0, "stop": 4, "step": 2}, [0, 2]),
     ],
 )
 def test_of_custom_range_function_work_with_objects_different_types(
-    args, expected_result
+    iterable, kwargs, expected_result
 ):
-    actual_result = custom_range(*args)
-
+    actual_result = custom_range(iterable, **kwargs)
     assert actual_result == expected_result
