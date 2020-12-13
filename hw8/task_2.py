3d4d50aa92e008bedc743d0cb5bc0301796e5da9
Aleks-E <aleks99979@bk.ru>
2020-12-13 19:50:26 +0300

    file replaced

--- hw8/task_2.py
+++ hw8/task_2.py
@@ -71,6 +71,7 @@ class TableData:
 
     def __getitem__(self, key: str):
         cursor = self.database_connection(f"SELECT name, * FROM {self.table_name}")
+
         data = True
         while data is not None:
             data = cursor.fetchone()
