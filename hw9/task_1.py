ca6a05bf461a4e0a145df78f87fefd067ba9877e
Aleks-E <aleks99979@bk.ru>
2020-12-15 23:16:59 +0300

    corrected conditional

--- hw9/task_1.py
+++ hw9/task_1.py
@@ -19,10 +19,9 @@ from typing import Iterator, List
 
 def merge_sorted_files(file_list: List) -> Iterator:
     def get_number(file_path: str) -> Iterator:
-        with open(file_path) as data_1:
-            for i in data_1:
-                i = i.rstrip()
-                yield i
+        with open(file_path) as text:
+            for line in text:
+                yield line.rstrip()
 
     number_iter_1 = iter(get_number(file_list[0]))
     number_iter_2 = iter(get_number(file_list[1]))
@@ -41,7 +40,7 @@ def merge_sorted_files(file_list: List) -> Iterator:
             except StopIteration:
                 ...
 
-        if (number_1 and number_2) is not None:
+        if number_1 is not None and number_2 is not None:
             if number_1 <= number_2:
                 result_number, number_1 = number_1, None
                 yield int(result_number)
@@ -57,5 +56,5 @@ def merge_sorted_files(file_list: List) -> Iterator:
             result_number, number_1 = number_1, None
             yield int(result_number)
 
-        elif (number_1 and number_2) is None:
+        elif number_1 is None and number_2 is None:
             break
