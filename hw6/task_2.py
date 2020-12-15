934c45547e1de0255f652bd58a672adaa4164d26
Aleks-E <aleks99979@bk.ru>
2020-12-15 23:51:18 +0300

    changed files

--- hw6/task_2.py
+++ hw6/task_2.py
@@ -46,8 +46,7 @@ def instances_counter(decorating_class: Type) -> "modified_class":
 
         @classmethod
         def reset_instances_counter(cls: "ModifiedClass") -> int:
-            cls.instance_count_old = cls.__instance_count
-            cls.__instance_count = 0
-            return cls.instance_count_old
+            instance_count_old, cls.__instance_count = cls.__instance_count, 0
+            return instance_count_old
 
     return ModifiedClass
