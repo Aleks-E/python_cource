87d3b2293dd28d8d093a414ad3df47ba0031de99
Aleks-E <aleks99979@bk.ru>
2021-01-03 13:19:44 +0300

    hw 12 task 1

--- hw12/task_1.py
+++ hw12/task_1.py
@@ -1,9 +1,6 @@
-import os
+import django
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
-print(BASE_DIR)
+print(django.get_version())
 
 
 
@@ -16,39 +13,50 @@ print(BASE_DIR)
 
 
 
-
-
-from django.db import models
-
-
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
-
-application = get_wsgi_application()
-
-
-
-
-
-
-class Person(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-
-
-class Teacher(Person):
-    dimploma = models.CharField(max_length=50)
-
-
-gumby = Person.get(101)
-gumby.last_name = 'ABCDEFG'
-gumby.save()
-gumby.dogs
-["Sheila", "Bobique", ]
+# import os
+#
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#
+#
+# print(BASE_DIR)
+#
+#
+#
+#
+#
+#
+#
+# from django.db import models
+#
+#
+#
+# import os
+#
+# from django.core.wsgi import get_wsgi_application
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+#
+# application = get_wsgi_application()
+#
+#
+#
+#
+#
+#
+# class Person(models.Model):
+#     first_name = models.CharField(max_length=30)
+#     last_name = models.CharField(max_length=30)
+#
+#
+# class Teacher(Person):
+#     dimploma = models.CharField(max_length=50)
+#
+#
+# gumby = Person.get(101)
+# gumby.last_name = 'ABCDEFG'
+# gumby.save()
+# gumby.dogs
+# ["Sheila", "Bobique", ]
 
 
 
