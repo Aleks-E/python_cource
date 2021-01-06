bb39bcb75fd0fa7b068fb0a9ed4162916814b511
Aleks-E <aleks99979@bk.ru>
2021-01-06 14:54:42 +0300

    files replaced

--- hw11/task_1.py
+++ hw11/task_1.py
@@ -1,198 +1,6 @@
-MyClass = type('MyClass', (object,), {'my_attribute': 0})
 """
-'MyClass' - имя класса
-(object,) - родители (может быть пустым)
-{'my_attribute': 0} - словарь атрибутов (может быть пустым)
-"""
-# print(type(MyClass))        # <class 'type'>
-# -------------------------------------
-
-def my_class_init(self, attr_value):
-    self.my_attribute = attr_value
-
-
-MyClass = type('MyClass', (object,), {'__init__': my_class_init})
-o = MyClass('test')
-# print(o.my_attribute)       # test
-
-
-
-# -------------------------------------
-"""
-Создание класса с методом
-"""
-def a1():
-    return 22
-
-A = type('A', (), {'a': a1})
-
-# print(A.a())    # 22
-# -------------------------------------
-
-
-def func(self, attr):
-    return attr ** 2
-
-A = type('A', (), {'func': func})
-
-a = A()
-
-# print(a.func(9))       # 81
-# -------------------------------------
-
-
-class A:
-    def __init__(self, attr_1, attr_2):
-        self.attr_1 = attr_1
-        self.attr_2 = attr_2
-
-    def upper(self):
-        my_list = []
-        for i in self.__dict__:
-            my_list.append(i)
-        for i in my_list:
-            setattr(self, i.upper(), self.__dict__[i])
-            delattr(self, i)
-
-
-a = A(22, 33)
-# print(a.__dict__)     # {'attr_1': 22, 'attr_2': 33}
-a.upper()
-# print(a.__dict__)     # {'ATTR_1': 22, 'ATTR_2': 33}
-# -------------------------------------
-
-
-class A:
-    def __init__(self):
-        print("A.__init__")
-        self.upper()
-
-    def upper(self):
-        my_list = []
-        for i in self.__dict__:
-            my_list.append(i)
-        for i in my_list:
-            setattr(self, i.upper(), self.__dict__[i])
-            delattr(self, i)
-
-
-class B(A):
-    def __init__(self, attr_1):
-        print("B.__init__")
-        self.attr_1 = attr_1
-        super().__init__()
-
-
-# a = B(11)
-# print(a.__dict__)
-# -------------------------------------
-
-
-class A:
-    def __init__(self):
-        self.keys = []
-        self.set_attr()
-
-    def set_attr(self):
-        for i in self.__dict__:
-            self.keys.append(self.__dict__[i])
-
-
-class B(A):
-    def __init__(self):
-        self.attr_1 = 'attr_1'
-        super().__init__()
-
-
-a = B()
-
-# print(a.__dict__)
-# -------------------------------------
-
-
-def my_class_init(self):
-    print("self")
-
-
-
-MyClass = type('MyClass', (), {'__init__': my_class_init})
-
-
-class NewClass(MyClass):
-    ...
-
-
-# a = NewClass()
-# -------------------------------------
-
-
-def my_metaclass(name, parents, attributes):
-    return 'Hello'
-
-
-class C(metaclass=my_metaclass):
-    ...
-
-
-# print(C)            # Hello
-# print(type(C))      # <class 'str'>
-# -------------------------------------
-
-
-def my_metaclass(name, parents, attributes):
-    for attr in attributes:
-        print(attr)
-
-
-# class C(metaclass=my_metaclass):
-#     attr_1 = 1
-
-# -------------------------------------
-
-def SimplifiedEnum(name, parents, attributes):
-    keys = []
-    for attr in attributes:
-        if not attr.startswith("_"):
-            keys.append(attributes[attr])
-
-    # print('name', name)
-    # print('parents', parents)
-    # print('attributes', attributes)
-
-    return type(name, parents, attributes | {'__keys': tuple(keys)})
-
-
-
-class ColorsEnum(metaclass=SimplifiedEnum):
-    RED = "RED"
-    BLUE = "BLUE"
-    ORANGE = "ORANGE"
-    BLACK = "BLACK"
-
-
-class SizesEnum(metaclass=SimplifiedEnum):
-    XL = "XL"
-    L = "L"
-    M = "M"
-    S = "S"
-    XS = "XS"
-
-
-
-# print(C.__dict__)
-
-# print(dir(C))
-
-# print(type(ColorsEnum))
-
-
-print(ColorsEnum.__keys)
-print(ColorsEnum.RED)
-
-print(SizesEnum.__keys)
-print(SizesEnum.XL)
-
-assert ColorsEnum.RED == "RED"
+Vasya implemented nonoptimal Enum classes.
+Remove duplications in variables declarations using metaclasses.
 
 from enum import Enum
 
@@ -204,18 +12,35 @@ class ColorsEnum(Enum):
     BLACK = "BLACK"
 
 
-
-# assert ColorsEnum.RED == "RED"      # AssertionError
-
-# print(ColorsEnum.RED)
-
-
-
-
+class SizesEnum(Enum):
+    XL = "XL"
+    L = "L"
+    M = "M"
+    S = "S"
+    XS = "XS"
 
 
+Should become:
+
+class ColorsEnum(metaclass=SimplifiedEnum):
+    __keys = ("RED", "BLUE", "ORANGE", "BLACK")
 
 
+class SizesEnum(metaclass=SimplifiedEnum):
+    __keys = ("XL", "L", "M", "S", "XS")
 
 
+assert ColorsEnum.RED == "RED"
+assert SizesEnum.XL == "XL"
+"""
 
+
+def simplified_enum(name: str, parents: tuple, attributes: dict) -> type:
+    new_attributes = []
+    for attr in attributes:
+        if not attr.startswith("_"):
+            new_attributes.append(attributes[attr])
+
+    return type(
+        name, parents, attributes | {"_" + name + "__keys": tuple(new_attributes)}
+    )
--- hw11/test_task_1.py
+++ hw11/test_task_1.py
@@ -1,5 +1,25 @@
+from hw11.task_1 import simplified_enum
+
+import pytest
 
 
+@pytest.fixture()
+def colors_enum():
+    class ColorsEnum(metaclass=simplified_enum):
+        RED = "RED"
+        BLUE = "BLUE"
+
+    return ColorsEnum
 
 
+def test_new_class_attributes(colors_enum):
+    assert colors_enum.RED == "RED"
+    assert colors_enum.BLUE == "BLUE"
 
+
+def test_create_attribute_collection(colors_enum):
+    assert "_ColorsEnum__keys" in dir(colors_enum)
+
+
+def test_attribute_collection(colors_enum):
+    assert colors_enum.__dict__["_ColorsEnum__keys"] == ("RED", "BLUE")
