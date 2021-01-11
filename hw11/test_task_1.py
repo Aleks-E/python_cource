5bae2f8e4ff4241da8271a0cee1e732bdb48313f
Aleks-E <aleks99979@bk.ru>
2021-01-11 18:48:41 +0300

    tests deleted

--- hw11/test_task_1.py
+++ hw11/test_task_1.py
@@ -15,11 +15,3 @@ def colors_enum():
 def test_new_class_attributes(colors_enum):
     assert colors_enum.RED == "RED"
     assert colors_enum.BLUE == "BLUE"
-
-
-def test_create_attribute_collection(colors_enum):
-    assert "_ColorsEnum__keys" in dir(colors_enum)
-
-
-def test_attribute_collection(colors_enum):
-    assert colors_enum.__dict__["_ColorsEnum__keys"] == ("RED", "BLUE")
